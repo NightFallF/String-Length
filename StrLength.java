d73403864298bd0904f91d616c0407a1c35da3eb
NightFallF <nightfallzadh@gmail.com>
2025-01-25 13:40:29 +0530

    Add files via upload

---
+++ StrLength.java
@@ -0,0 +1,6 @@
+public class StrLength {
+    public static void main(String[] args) {
+        String book = "Human Nature";
+        System.out.printf("The length of the String is: %d", book.length());
+    }
+}
